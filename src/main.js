5499dd7b9931a3e619b972ba63404ae957c27d1f
TruCoder2333 <bondkyrylo@gmail.com>
2023-09-24 00:32:33 +0300

    Added Startpage

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from 'D:/meduzzen_front/src/pages/App.vue'
+
+createApp(App).mount('#app')
---
+++ src/pages/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <HelloWorld msg="Hello mentor"/>
+</template>
+
+<script>
+import HelloWorld from 'D:/meduzzen_front/src/components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Times New Roman', Times, serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #0e0224;
+  margin-top: 60px;
+}
+</style>
